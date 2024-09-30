c2030ec18780f07411fc7cc69865bf213340f8bf
corwindark <corwindark.spam@gmail.com>
2024-09-30 15:45:52 -0400

    building multiple windows

--- src/battlespace_testing/battlespace_testing.py
+++ src/battlespace_testing/battlespace_testing.py
@@ -3,36 +3,28 @@ import arcade
 
 shop_window_size = (600, 800)
 battle_window_size = (800, 800)
+overall_window_size = (1000,1000)
 
 class ShopView(arcade.View):
 
     def on_show_view(self):
+        arcade.set_background_color(arcade.color.WHITE)
 
     def on_draw(self):
+        self.clear()
+        arcade.draw_text("SHOP SCRN", shop_window_size[0], shop_window_size[1] )
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
+        self.clear()
+        arcade.close_window()
+
 
 def main():
 
-
-
-
-arcade.open_window(600,600, "example")
-
-arcade.set_background_color(arcade.csscolor.SKY_BLUE)
-
-arcade.start_render()   
-
-arcade.draw_lrtb_rectangle_filled(200, 300, 200, 100, arcade.csscolor.GREEN)
-
-arcade.draw_text("5/5", 250, 150, arcade.color.BLACK, 18)
-
-arcade.finish_render()
-
-arcade.run()
-
-
-
+    window = arcade.Window(overall_window_size[0], overall_window_size[1], 'test')
+    shop1_view = ShopView()
+    window.show_view(shop1_view)
+    arcade.run()
 
 
 if __name__ == '__main__':
